a93c6a6dd7aaaff9d84d7673039028b4d3b4b403
codebysid <codebysidd@gmail.com>
2024-03-20 21:59:16 +0530

    headers

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const PORT = 8000
 
 const app = express()
 
+function setCorsHeaders(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}
+
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN
+  origin: process.env.FRONTEND_ORIGIN,
+  optionsSuccessStatus: 200,
+  allowedheaders
 }))
+
+app.use(setCorsHeaders)
 app.use(express.json())
 
 app.use("/api/codesubmission", codeSubmissionRouter)
