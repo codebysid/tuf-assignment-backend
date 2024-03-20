860a8ac2d879e93d3a13105396ad967060b920ae
codebysid <codebysidd@gmail.com>
2024-03-20 23:14:19 +0530

    typo

--- controller/codeSubmission.controller.js
+++ controller/codeSubmission.controller.js
@@ -7,22 +7,30 @@ async function saveCodeSubmission(req, res) {
   if (!username || !sourceCode || !codeLanguage) throw new Error("Details required")
   try {
     createConnection(db).query(saveToSqlQuery, [username, codeLanguage, sourceCode, stdIn], (err) => {
-      if (err) throw new Error(err)
+      if (err) {
+        console.log(err)
+        return res.status(402)
+      }
       return res.status(200).json({ msg: "saved" })
     })
   } catch (err) {
-    throw new Error(err)
+    console.log(err)
+    return res.status(402)
   }
 }
 
 async function readCodeSubmissions(req, res) {
   try {
     createConnection(db).query(readSubmissionsQuery, (err, result) => {
-      if (err) throw new Error(err.message)
-      res.json({ msg: "Done", data: result })
+      if (err) {
+        console.log(err)
+        return res.status(402)
+      }
+      return res.json({ msg: "Done", data: result }).status(200)
     })
   } catch (err) {
     console.log(err.message)
+    return res.status(402)
   }
 }
 
