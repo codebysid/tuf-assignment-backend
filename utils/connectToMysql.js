48db5ab242c11412a4b96fadfdfec365b2d16be3
codebysid <codebysidd@gmail.com>
2024-03-21 17:10:31 +0530

    port

--- utils/connectToMysql.js
+++ utils/connectToMysql.js
@@ -4,6 +4,7 @@ const db = {
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT
 }
 
 module.exports = { db }
